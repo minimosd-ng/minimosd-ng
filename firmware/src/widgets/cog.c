136ecd8cb446e2e18146510ed903ec92a2d6461d
Luis Alves <ljalvs@gmail.com>
2014-12-18 22:13:56 +0000

    Add missing COG widget file.
    Signed-off-by: Luis Alves <ljalvs@gmail.com>

---
+++ firmware/src/widgets/cog.c
@@ -0,0 +1,51 @@
+/*******************************************************************
+
+MinimOSD-ng
+Copyright (C) 2014  Luis Alves
+
+This file is part of MinimOSD-ng.
+
+MinimOSD-ng is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+MinimOSD-ng is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MinimOSD-ng.  If not, see <http://www.gnu.org/licenses/>.
+
+*********************************************************************/
+
+#include "config.h"
+#include <stdio.h>
+#include <string.h>
+#include "widgets.h"
+#include "max7456.h"
+#include "mavlink.h"
+
+#define DEBUG 0
+#if DEBUG
+#define PRINTF(...) printf(__VA_ARGS__)
+#else
+#define PRINTF(...)
+#endif
+
+WIDGET_STATE(0, 0, WIDGET_DISABLED);
+
+extern struct mavlink_data mavdata;
+
+static void draw(void)
+{
+  char buf[10];
+  char idx = ((int) (((mavdata.gps_cog/100) - mavdata.hud_heading) * 16) / 360);
+  idx = (idx & 0xf) << 1;
+  sprintf(buf, "%c%c", 0x90+idx, 0x91+idx);
+  max7456_puts(state.x, state.y, buf);
+}
+
+WIDGET_DECLARE(cog_widget, "COG", COG_WIDGET_ID,  draw);
+
