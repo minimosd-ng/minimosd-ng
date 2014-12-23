48728f47375063c0f15727e06487e629174fff2e
Luis Alves <ljalvs@gmail.com>
2014-12-23 02:40:09 +0000

    Add more special font charater definitions
    Signed-off-by: Luis Alves <ljalvs@gmail.com>

--- firmware/src/max7456.h
+++ firmware/src/max7456.h
@@ -2,7 +2,10 @@
 #define __MAX7456_H__
 
 /* special font charaters */
-#define MAX7456_DIR_ARROWS 0x90
+#define MAX7456_FONT_DIR_ARROWS   (0x90)
+#define MAX7456_FONT_HOME         (0x0b)
+#define MAX7456_FONT_METERS       (0x0c)
+#define MAX7456_FONT_KILOMETERS   (0x1b)
 
 
 typedef enum {
